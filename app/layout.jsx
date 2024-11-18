b68e35c875e631c1c88b2beddb56cc7e1c18880a
adhasanblog <fhasanblog@gmail.com>
2024-11-18 13:54:57 +0700

    initial commit

---
+++ app/layout.jsx
@@ -0,0 +1,12 @@
+export default function Layout({children}) {
+    return (
+        <html>
+        <head>
+            <title>Learn NextJS Fundamentalck</title>
+        </head>
+        <body>
+        {children}
+        </body>
+        </html>
+    )
+}
